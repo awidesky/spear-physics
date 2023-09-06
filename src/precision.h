df73f4415090106fad73328e8a293f896a19cdf9
Eugene Hong <awidesky@naver.com>
2023-09-06 13:15:43 +0900

    fix spear_config.h

--- src/precision.h
+++ src/precision.h
@@ -17,11 +17,11 @@ namespace spear {
 #	define sp_invsqrt invsqrt
 #endif // SP_EXPERIMENTAL_QUAKE_FASTINVSQRT
 
-#if SP_REAL_PRECISION == "float_4"
+#if SP_REAL_PRECISION == float_4
 	typedef float real;
 #	define real_sqrt sqrtf
 #	define real_inverse_sqrt sp_invsqrtf
-#elif SP_REAL_PRECISION == "double_8"
+#elif SP_REAL_PRECISION == double_8
 	typedef double real;
 #	define real_sqrt sqrt
 #	define real_inverse_sqrt sp_invsqrt
